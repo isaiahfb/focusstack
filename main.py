c1a39e7eaa86908b8e6c21ecb0ee8325a383689e
isaiahfb <isaiah.fujii@gmail.com>
2020-04-21 13:41:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+#
+# read in a set of images with different focus points
+#
+#
 
+#
+# align images
+#
+#
+
+#
+# guassian blur images
+#
+#
+
+#
+# calculate laplacian of each image
+#
+#
+
+#
+# for each (x, y) point, choose the pixel with the greatest absolute value of laplacian from the stack and place it in new image
+#
+#
+
+#
+# output created image
+#
+#
